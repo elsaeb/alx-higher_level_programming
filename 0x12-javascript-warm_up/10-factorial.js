ab15fa3257b1a78cbd3782f54c476169b7073602
elsab <elsabiru8@gmail.com>
2023-03-15 21:46:21 +0300

    therest of the tasks

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function rec(x){
+    if (x === 0){
+	return(1);
+    }else
+    {
+	return (rec(x-1)*x);
+    }
+}
+
+const x = Number(process.argv[2]);
+if(x) {
+    const result = rec(x);
+    console.log(result);
+}else{
+    console.log(1);
+}
